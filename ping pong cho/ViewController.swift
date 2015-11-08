e5ad09179e679e44378ea96dc2e8a66ed8661428
Felipe Osorio <felipeaosorio@gmail.com>
2015-11-08 10:09:24 -0500

    UI design element updates

--- ping pong cho/ViewController.swift
+++ ping pong cho/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playerOneName: UITextField!
+    @IBOutlet weak var playerOneGame: UIButton!
+    @IBOutlet weak var playerOneCho: UIStepper!
+    
+    @IBOutlet weak var playerTwoName: UITextField!
+    @IBOutlet weak var playerTwoGame: UIButton!
+    @IBOutlet weak var playerTwoCho: UIStepper!
+    
+    
+    @IBOutlet weak var gameProgressBar: UIProgressView!
+    @IBOutlet weak var oldSchoolOn: UISwitch!
+    @IBOutlet weak var pingPongLogo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +33,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func oneGamePress(sender: UIButton) {
+    }
+    @IBAction func twoGamePress(sender: UIButton) {
+    }
+    
 }
 
