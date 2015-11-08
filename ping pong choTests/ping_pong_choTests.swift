ebba1a0996015ba297db2a01ea1a5e57da47beb8
Felipe Osorio <felipeaosorio@gmail.com>
2015-11-07 19:29:58 -0500

    Initial Commit

---
+++ ping pong choTests/ping_pong_choTests.swift
@@ -0,0 +1,36 @@
+//
+//  ping_pong_choTests.swift
+//  ping pong choTests
+//
+//  Created by Felipe Alejandro Osorio on 11/7/15.
+//  Copyright © 2015 Felipe Osorio. All rights reserved.
+//
+
+import XCTest
+@testable import ping_pong_cho
+
+class ping_pong_choTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
