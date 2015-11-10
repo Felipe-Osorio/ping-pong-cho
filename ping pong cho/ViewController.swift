877255906ef9472cab6f7473ec1ba0fbee5ffe8d
Felipe Osorio <felipeaosorio@gmail.com>
2015-11-10 00:33:01 -0500

    connecting UI elements, adding stepper functionality

--- ping pong cho/ViewController.swift
+++ ping pong cho/ViewController.swift
@@ -10,23 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var playerOneScore = 0
-    var playerTwoScore = 0
+    //var playerOneScore = 0
+    //var playerTwoScore = 0
     var playerOneGames = 0
     var playerTwoGames = 0
 
-    @IBOutlet weak var playerOneName: UITextField!
+    @IBOutlet weak var playerOneScore: UITextField!
     @IBOutlet weak var playerOneGame: UIButton!
     @IBOutlet weak var playerOneCho: UIStepper!
     
-    @IBOutlet weak var playerTwoName: UITextField!
+    @IBOutlet weak var playerTwoScore: UITextField!
     @IBOutlet weak var playerTwoGame: UIButton!
     @IBOutlet weak var playerTwoCho: UIStepper!
     
     
     @IBOutlet weak var gameProgressBar: UIProgressView!
     @IBOutlet weak var oldSchoolOn: UISwitch!
-    @IBOutlet weak var pingPongLogo: UIImageView!
+    //@IBOutlet weak var pingPongLogo: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,14 +38,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func oneGamePress(sender: UIButton!) {
-        ++playerOneScore
-        print(playerOneScore)
+    @IBAction func oneGamePress(sender: UIButton!) { //won game
+        ++playerOneGames
+        print(playerOneGames)
     }
 
-    @IBAction func twoGamePress(sender: UIButton!) {
-        ++playerTwoScore
+    @IBAction func twoGamePress(sender: UIButton!) { //won game
+        ++playerTwoGames
+        print(playerTwoGames)
     }
-    
+    @IBAction func oneClicker(sender: UIStepper) { //increment score
+        self.playerOneScore.text = Int(sender.value).description
+    }
+    @IBAction func twoClicker(sender: UIStepper) { //increment score
+        self.playerTwoScore.text = Int(sender.value).description
+    }
 }
+
+
+
 
